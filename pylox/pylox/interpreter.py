709284d31f9edf45d078684d2cb3326b555b044c
hieutdle <hieu.tdle@gmail.com>
2025-01-14 18:15:36 +0100

    fix match enum import mismatch

--- pylox/pylox/interpreter.py
+++ pylox/pylox/interpreter.py
@@ -1,10 +1,13 @@
 import pylox.expr as ast
 import typing
-from error import LoxRuntimeError
-from tokens import Token, TokenType
+from pylox.error import LoxRuntimeError
+from pylox.tokens import Token, TokenType
 
 
 class Interpreter(ast.ExprVisitor):
+    def interpret(self, expr: ast.Expr):
+        return self.stringify(self.evaluate(expr))
+
     def evaluate(self, expr: ast.Expr) -> typing.Any:
         return expr.accept(self)
 
@@ -29,6 +32,15 @@ class Interpreter(ast.ExprVisitor):
     def visit_binary_expr(self, expr: ast.Binary) -> typing.Any:
         left = self.evaluate(expr.left)
         right = self.evaluate(expr.right)
+
+        # print(f"Operator: {expr.operator.token_type}, Expected: {TokenType.PLUS}")
+        # print(f"ID of expr.operator.token_type: {id(expr.operator.token_type)}")
+        # print(f"ID of TokenType.PLUS: {id(TokenType.PLUS)}")
+        # ID of expr.operator.token_type: 140211793605728
+        # ID of TokenType.PLUS: 140211793606544
+        # Ensure correct, consistent import
+        # from pylox.tokens import TokenType
+        # not from tokens import TokenType
 
         match expr.operator.token_type:
             case TokenType.GREATER:
@@ -59,14 +71,14 @@ class Interpreter(ast.ExprVisitor):
                 if self.is_number(left) and self.is_number(right):
                     return float(left) + float(right)
 
-                if type(left) is str and type(right) is str:
-                    return left + right
+                if type(left) is str or type(right) is str:
+                    return str(left) + str(right)
 
                 raise LoxRuntimeError(
                     expr.operator,
-                    "Operands must be two numbers of two strings.",
+                    "Operands must be numbers or strings.",
                 )
-        return None  # Fallback case
+        return "abc"
 
     @staticmethod
     def is_truthy(object: typing.Any) -> bool:
@@ -92,3 +104,11 @@ class Interpreter(ast.ExprVisitor):
         if Interpreter.is_number(left) and Interpreter.is_number(right):
             return
         raise LoxRuntimeError(op, "Operands must be a numbers.")
+
+    @staticmethod
+    def stringify(value: typing.Any) -> str:
+        if value is None:
+            return "nil"
+        if type(value) is float and float(value).is_integer():
+            return str(int(value))
+        return str(value)
